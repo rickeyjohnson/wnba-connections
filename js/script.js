2895d1f92b0e862539f83fd64c9e077d919d5fac
rickeyjohnson <rdjwinter2004@gmail.com>
2024-08-01 21:20:50 -0400

    deselect button listens for events properly

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const cards = document.querySelectorAll(".card")
+const deselectButton = document.querySelector("#deselect")
 
 cards.forEach((card) => {
     card.addEventListener("click", (event) => {
         card.classList.toggle("selected")
     })
+})
+
+deselectButton.addEventListener("click", (event) => {
+    console.log('deselect btn works!')
 })
